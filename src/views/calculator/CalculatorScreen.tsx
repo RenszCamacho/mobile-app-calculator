23852a7688e6f3d2666ef74090ba27dc17595237
Renszo Camacho <varsovia84@gmail.com>
2021-11-10 21:27:29 +0100

    Set the onPressButton function.

--- src/views/calculator/CalculatorScreen.tsx
+++ src/views/calculator/CalculatorScreen.tsx
@@ -10,9 +10,16 @@ const CalculatorScreen = () => {
 
   const [digitNumber, setDigitNumber] = useState('0');
 
-  // const onPressButton = (value: string) => {
-  //   setDigitNumber(digitNumber + value);
-  // };
+  const onPressButton = (value: string) => {
+    if (value === '.' && digitNumber.includes('.')) {
+      return;
+    }
+    if (value === '.' && digitNumber === '0') {
+      setDigitNumber(digitNumber + value);
+      return;
+    }
+    setDigitNumber(digitNumber === '0' ? '' + value : digitNumber + value);
+  };
 
   const onPressClear = () => {
     setDigitNumber('0');
@@ -20,8 +27,20 @@ const CalculatorScreen = () => {
 
   return (
     <View style={container}>
-      <Text style={[operand, previousOperand]}>{digitNumber}</Text>
-      <Text style={[operand, currentOperand]}>{digitNumber}</Text>
+      {digitNumber !== '0' && (
+        <Text
+          numberOfLines={1}
+          adjustsFontSizeToFit
+          style={[operand, previousOperand]}>
+          {digitNumber}
+        </Text>
+      )}
+      <Text
+        numberOfLines={1}
+        adjustsFontSizeToFit
+        style={[operand, currentOperand]}>
+        {digitNumber}
+      </Text>
 
       <View style={wrapperButtons}>
         <Button color="#9B9B9B" value="C" action={onPressClear} />
@@ -31,29 +50,29 @@ const CalculatorScreen = () => {
       </View>
 
       <View style={wrapperButtons}>
-        <Button value="7" />
-        <Button value="8" />
-        <Button value="9" />
+        <Button value="7" action={onPressButton} />
+        <Button value="8" action={onPressButton} />
+        <Button value="9" action={onPressButton} />
         <Button color="#FF9427" value="x" />
       </View>
 
       <View style={wrapperButtons}>
-        <Button value="4" />
-        <Button value="5" />
-        <Button value="6" />
+        <Button value="4" action={onPressButton} />
+        <Button value="5" action={onPressButton} />
+        <Button value="6" action={onPressButton} />
         <Button color="#FF9427" value="-" />
       </View>
 
       <View style={wrapperButtons}>
-        <Button value="1" />
-        <Button value="2" />
-        <Button value="3" />
+        <Button value="1" action={onPressButton} />
+        <Button value="2" action={onPressButton} />
+        <Button value="3" action={onPressButton} />
         <Button color="#FF9427" value="+" />
       </View>
 
       <View style={wrapperButtons}>
-        <Button value="0" isBig />
-        <Button value="." />
+        <Button value="0" isBig action={onPressButton} />
+        <Button value="." action={onPressButton} />
         <Button color="#FF9427" value="=" />
       </View>
     </View>
